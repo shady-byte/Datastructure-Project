6ed33ac53c9dc1cbb5d8b60d19c2656a9ac00519
Shady Ali <shadyaly286@gmail.com>
2020-12-22 14:10:20 +0200

    updated version

--- bin/main.dart
+++ bin/main.dart
@@ -5,6 +5,17 @@ import 'queueClass.dart';
 import 'stackClass.dart';
 
 void main() {
+  List<dynamic> testData=[
+    'hii',
+    'a',
+    6,
+    0,
+    4.2,
+    'that ',
+    -7,
+    -5.1,
+    130
+  ];
   Queue queue;
   Stack stack;
   var count=1;
@@ -18,18 +29,18 @@ void main() {
   void queueTesting() {
     group('queue testing cases', (){
       test('queue.enqueue() is a test for function enqueue', () {
-        var value= random.nextInt(100000);
+        var value= testData[random.nextInt(9)];
         expect(queue.enqueue(value), equals((true)));
       });
 
       test('queue.dequeue() is a test for function dequeue', () {
-        var value= random.nextInt(100000);
+        var value= testData[random.nextInt(9)];
         queue.enqueue(value);
         expect(queue.dequeue(), equals((value)));
       });
 
       test('queue.getLength() is a test for function to get length ', () {
-        List<int> values=[random.nextInt(100000),random.nextInt(100000),random.nextInt(100000)];
+        List<dynamic> values=[testData[random.nextInt(9)],testData[random.nextInt(9)],testData[random.nextInt(9)]];
         queue.enqueue(values[0]);
         queue.enqueue(values[1]);
         queue.enqueue(values[2]);
@@ -37,7 +48,7 @@ void main() {
       });
 
       test('queue.peek() is a test for function to return head of queue ', () {
-        List<int> values=[random.nextInt(100000),random.nextInt(100000),random.nextInt(100000)];
+        List<dynamic> values=[testData[random.nextInt(9)],testData[random.nextInt(9)],testData[random.nextInt(9)]];
         queue.enqueue(values[0]);
         queue.enqueue(values[1]);
         queue.enqueue(values[2]);
@@ -45,7 +56,7 @@ void main() {
       });
 
       test('queue.isEmpty() is a test for function if it is empty or not ', () {
-        List<int> values=[random.nextInt(100000),random.nextInt(100000)];
+        List<dynamic> values=[testData[random.nextInt(9)],testData[random.nextInt(9)]];
         queue.enqueue(values[0]);
         queue.enqueue(values[1]);
         expect(queue.isEmpty(), equals((false)));
@@ -56,25 +67,25 @@ void main() {
   void stackTesting() {
     group('stack testing cases', (){
       test('stack.push() is a test for function push', () {
-        var value= random.nextInt(100000);
+        var value= testData[random.nextInt(9)];
         expect(stack.push(value), equals((true)));
       });
 
       test('stack.pop() is a test for function pop', () {
-        var value= random.nextInt(100000);
+        var value= testData[random.nextInt(9)];
         stack.push(value);
         expect(stack.pop(), equals((value)));
       });
 
       test('stack.getLength() is a test for function to get length ', () {
-        List<int> values=[random.nextInt(100000),random.nextInt(100000)];
+        List<dynamic> values=[testData[random.nextInt(9)],testData[random.nextInt(9)]];
         stack.push(values[0]);
         stack.push(values[1]);
         expect(stack.getLength(), equals((values.length)));
       });
 
       test('satck.peek() is a test for function to return head of stack ', () {
-        List<int> values=[random.nextInt(100000),random.nextInt(100000),random.nextInt(100000)];
+        List<dynamic> values=[testData[random.nextInt(9)],testData[random.nextInt(9)],testData[random.nextInt(9)]];
         stack.push(values[0]);
         stack.push(values[1]);
         stack.push(values[2]);
@@ -82,7 +93,7 @@ void main() {
       });
 
       test('stack.isEmpty() is a test for function if it is empty or not ', () {
-        List<int> values=[random.nextInt(100000),random.nextInt(100000)];
+        List<dynamic> values=[testData[random.nextInt(9)],testData[random.nextInt(9)]];
         stack.push(values[0]);
         stack.push(values[1]);
         expect(stack.isEmpty(), equals((false)));
